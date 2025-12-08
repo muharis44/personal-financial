09233f4cd84e2e221253d19ae0c941819362b6e0
muharis44 <68917449+muharis44@users.noreply.github.com>
2025-12-08 12:28:37 +0700

    Updated progress-bar.tsx

--- components/progress-bar.tsx
+++ components/progress-bar.tsx
@@ -2,23 +2,29 @@
 
 import { useEffect } from "react"
 import { usePathname } from "next/navigation"
-import NProgress from "nprogress"
-import "nprogress/nprogress.css"
 
-NProgress.configure({
-  showSpinner: false,
-  trickleSpeed: 100,
-  minimum: 0.2,
-})
+let NProgress: any = null
+if (typeof window !== "undefined") {
+  NProgress = require("nprogress")
+  NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+    minimum: 0.2,
+  })
+}
 
 export function ProgressBarProvider() {
   const pathname = usePathname()
 
   useEffect(() => {
-    NProgress.done()
+    if (NProgress) {
+      NProgress.done()
+    }
 
     return () => {
-      NProgress.start()
+      if (NProgress) {
+        NProgress.start()
+      }
     }
   }, [pathname])
 
